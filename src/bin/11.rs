6323430f7291d02813d2020b7f564d5211085e42
Lukas Dams <lukas.dams@proton.me>
2024-12-16 09:21:10 +0100

    Make day 11 a bit leaner

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -6,16 +6,16 @@ pub fn part_one(input: &str) -> Option<u64> {
     let initial_stones = parse_stones(input);
     const NUMBER_OF_BLINKS: u64 = 25;
     Some(initial_stones.iter()
-        .map(|stone_number| calculate_amount_of_stones(*stone_number, NUMBER_OF_BLINKS))
+        .map(|stone_number| num_stones_after_blinks(*stone_number, NUMBER_OF_BLINKS))
         .sum())
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     let initial_stones = parse_stones(input);
     const NUMBER_OF_BLINKS: u64 = 75;
-    let mut cache = HashMap::new();
+    let mut cache: HashMap<(u64, u64), u64> = HashMap::new();
     Some(initial_stones.iter()
-        .map(|stone_number| calculate_amount_of_stones_using_cache(*stone_number, NUMBER_OF_BLINKS, &mut cache))
+        .map(|stone_number| num_stones_after_blinks_optimized(*stone_number, NUMBER_OF_BLINKS, &mut cache))
         .sum())
 }
 
@@ -28,13 +28,13 @@ fn parse_stones(input: &str) -> Vec<u64> {
     initial_stones
 }
 
-fn calculate_amount_of_stones(stone_number: u64, number_of_blinks: u64) -> u64 {
+fn num_stones_after_blinks(stone_number: u64, number_of_blinks: u64) -> u64 {
     if number_of_blinks == 0 {
         return 1
     }
 
     if stone_number == 0 {
-        return calculate_amount_of_stones(1, number_of_blinks - 1);
+        return num_stones_after_blinks(1, number_of_blinks - 1);
     }
 
     let log10 = stone_number.ilog10();
@@ -43,33 +43,28 @@ fn calculate_amount_of_stones(stone_number: u64, number_of_blinks: u64) -> u64 {
         let divisor: u64 = 10_u64.pow(log10 / 2 + 1);
         let first_half = stone_number / divisor;
         let second_half = stone_number % divisor;
-        let amount_of_stones_first_half_after_blink = calculate_amount_of_stones(first_half, number_of_blinks - 1);
-        let amount_of_stones_second_half_after_blink = calculate_amount_of_stones(second_half, number_of_blinks - 1);
-        let amount_of_stones_after_blink = amount_of_stones_first_half_after_blink + amount_of_stones_second_half_after_blink;
-        return amount_of_stones_after_blink;
+        let num_stones_first_half_after_blink = num_stones_after_blinks(first_half, number_of_blinks - 1);
+        let num_stones_second_half_after_blink = num_stones_after_blinks(second_half, number_of_blinks - 1);
+        let num_stones_after_blink = num_stones_first_half_after_blink + num_stones_second_half_after_blink;
+        return num_stones_after_blink;
     }
 
-    calculate_amount_of_stones(stone_number * 2024, number_of_blinks - 1)
+    num_stones_after_blinks(stone_number * 2024, number_of_blinks - 1)
 }
 
-fn calculate_amount_of_stones_using_cache(stone_number: u64, number_of_blinks: u64, cache: &mut HashMap<u64, HashMap<u64, u64>>) -> u64 {
-    if number_of_blinks == 0 {
+fn num_stones_after_blinks_optimized(stone_number: u64, num_blinks: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
+    if num_blinks == 0 {
         return 1
     }
 
-    let amount_of_stones = cache
-        .entry(stone_number)
-        .or_insert_with(HashMap::new)
-        .get(&number_of_blinks);
-
-    if let Some(amount_of_stones) = amount_of_stones {
-        return *amount_of_stones;
+    if let Some(num_stones) = cache.get(&(stone_number, num_blinks)) {
+        return *num_stones;
     }
 
     if stone_number == 0 {
-        let amount_of_stones_after_blink = calculate_amount_of_stones_using_cache(1, number_of_blinks - 1, cache);
-        cache.get_mut(&stone_number).unwrap().insert(number_of_blinks, amount_of_stones_after_blink);
-        return amount_of_stones_after_blink;
+        let num_stones = num_stones_after_blinks_optimized(1, num_blinks - 1, cache);
+        cache.insert((stone_number, num_blinks), num_stones);
+        return num_stones;
     }
 
     let log10 = stone_number.ilog10();
@@ -78,16 +73,16 @@ fn calculate_amount_of_stones_using_cache(stone_number: u64, number_of_blinks: u
         let divisor: u64 = 10_u64.pow(log10 / 2 + 1);
         let first_half = stone_number / divisor;
         let second_half = stone_number % divisor;
-        let amount_of_stones_first_half_after_blink = calculate_amount_of_stones_using_cache(first_half, number_of_blinks - 1, cache);
-        let amount_of_stones_second_half_after_blink = calculate_amount_of_stones_using_cache(second_half, number_of_blinks - 1, cache);
-        let amount_of_stones_after_blink = amount_of_stones_first_half_after_blink + amount_of_stones_second_half_after_blink;
-        cache.get_mut(&stone_number).unwrap().insert(number_of_blinks, amount_of_stones_after_blink);
-        return amount_of_stones_after_blink;
+        let second_half_num_stones = num_stones_after_blinks_optimized(first_half, num_blinks - 1, cache);
+        let first_half_num_stones = num_stones_after_blinks_optimized(second_half, num_blinks - 1, cache);
+        let num_stones = second_half_num_stones + first_half_num_stones;
+        cache.insert((stone_number, num_blinks), num_stones);
+        return num_stones;
     }
 
-    let amount_of_stones_after_blink = calculate_amount_of_stones_using_cache(stone_number * 2024, number_of_blinks - 1, cache);
-    cache.get_mut(&stone_number).unwrap().insert(number_of_blinks, amount_of_stones_after_blink);
-    amount_of_stones_after_blink
+    let num_stones = num_stones_after_blinks_optimized(stone_number * 2024, num_blinks - 1, cache);
+    cache.insert((stone_number, num_blinks), num_stones);
+    num_stones
 }
 
 #[cfg(test)]
@@ -95,29 +90,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_calculate_amount_of_stones() {
-        let result = calculate_amount_of_stones(125, 6);
+    fn test_num_stones() {
+        let result = num_stones_after_blinks(125, 6);
         assert_eq!(result, 7);
     }
 
     #[test]
-    fn test_calculate_amount_of_stones_using_cache() {
-        let result = calculate_amount_of_stones_using_cache(125, 6, &mut HashMap::new());
+    fn test_num_stones_using_cache() {
+        let result = num_stones_after_blinks_optimized(125, 6, &mut HashMap::new());
         assert_eq!(result, 7);
     }
 
     #[test]
-    fn test_calculate_amount_of_stones_with_more() {
-        let first_result = calculate_amount_of_stones(125, 25);
-        let second_result = calculate_amount_of_stones(17, 25);
+    fn test_num_stones_with_more() {
+        let first_result = num_stones_after_blinks(125, 25);
+        let second_result = num_stones_after_blinks(17, 25);
         assert_eq!(first_result + second_result, 55312);
     }
 
     #[test]
-    fn test_calculate_amount_of_stones_using_cache_with_more() {
+    fn test_num_stones_using_cache_with_more() {
         let mut cache = HashMap::new();
-        let first_result = calculate_amount_of_stones_using_cache(125, 25, &mut cache);
-        let second_result = calculate_amount_of_stones_using_cache(17, 25, &mut cache);
+        let first_result = num_stones_after_blinks_optimized(125, 25, &mut cache);
+        let second_result = num_stones_after_blinks_optimized(17, 25, &mut cache);
         assert_eq!(first_result + second_result, 55312);
     }
 
